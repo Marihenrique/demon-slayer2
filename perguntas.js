283ea4c8e548fc229668d11d0cdc8edb99e400d9
Marihenrique <117747224+Marihenrique@users.noreply.github.com>
2024-09-04 20:33:24 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,135 @@
+export const perguntas = [
+    {
+        enunciado: "1. Quais ferramentas você usaria?",
+        alternativas: [
+            {
+                texto: "A) Uma Katana",
+                afirmacao: [
+                    " O Tanjiro Kamado. Você é alguém gentil e determinado. Você tem uma personalidade forte com o objetivo de exterminar qualquer ameaça aos humanos como onis, faria tudo para salvar seus amigos"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B)Duas katanas",
+                afirmacao: [
+                    "O Inosuke Hashibira. Você é uma pessoas bem extravagante, voce é conhecido pelo seu temperamento e que atacaria uma ameaça com tudo de si sem nem pensar, tentaria acabar logo com o inimigo mesmo extremamente ferido."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "2. Você se considera :",
+        alternativas: [
+            {
+                texto: "Uma pessoa calma",
+                afirmacao: [
+                    "O Tanjiro Kamado. Você é alguém gentil e determinado. Você tem uma personalidade forte com o objetivo de exterminar qualquer ameaça aos humanos como onis, faria tudo para salvar seus amigos",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " Uma pessoa mais agitada",
+                afirmacao: [
+                    "O Inosuke Hashibira. Você é uma pessoas bem extravagante, voce é conhecido pelo seu temperamento e que atacaria uma ameaça com tudo de si sem nem pensar, tentaria acabar logo com o inimigo mesmo extremamente ferido."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Você se considera mais:",
+        alternativas: [
+            {
+                texto: "Uma pessoa Flexível",
+                afirmacao: [
+                    "O Inosuke Hashibira. Você é uma pessoas bem extravagante, voce é conhecido pelo seu temperamento e que atacaria uma ameaça com tudo de si sem nem pensar, tentaria acabar logo com o inimigo mesmo extremamente ferido."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Uma pessoa Ágil",
+                afirmacao: [
+                    " O Tanjiro Kamado. Você é alguém gentil e determinado. Você tem uma personalidade forte com o objetivo de exterminar qualquer ameaça aos humanos como onis, faria tudo para salvar seus amigos"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual a sua respiração favorita?",
+        alternativas: [
+            {
+                texto: "Respiração do trovão.",
+                afirmacao: [
+                    "O Zenitsu Agatsuma. Você sempre tenta superar as expectativas é uma pesssoa muito esforçada, muitas vezes você pode parecer assustado, sempre em busca de um par e disposto a tudo para salvar a quem ama."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Respiração da Água",
+                afirmacao: [
+                    " O Tomioka Giyu. Você é uma pessoa reservada e com um forte senso de justiça, sem muita tolerância, uma pessoa que sempre age com muita seriedade. Porém ddará sempre tudo de si independente da situação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você fosse um oni, você seria...",
+        alternativas: [
+            {
+                texto: "Um oni do mal",
+                afirmacao: [
+                    "O Muzan Kibutsuji. é uma pessoa insensível e implacado, tendo um intelecto excepcional, você faz de tudo para sobreviver e conseguir alcançar seus objetivos."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Um oni do bem",
+                afirmacao: [
+                    "A Nezuko Kamado. Você é uma pessoa muito doce, gentil e adorável, uma pessoa que costuma falar pouco, tem muita empatia e carinho e custuma sempre proteger tudo e todos."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você visse um oni o que você faria?",
+        alternativas: [
+            {
+                texto: " Sairia correndo",
+                afirmacao: [
+                    "O Zenitsu Agatsuma. Você sempre tenta superar as expectativas é uma pesssoa muito esforçada, muitas vezes você pode parecer assustado, sempre em busca de um par e disposto a tudo para salvar a quem ama."
+                ],
+            },
+            {
+                texto: "O enfrentaria",
+                afirmacao: [
+                    "O Inosuke Hashibira. Você é uma pessoas bem extravagante, voce é conhecido pelo seu temperamento e que atacaria uma ameaça com tudo de si sem nem pensar, tentaria acabar logo com o inimigo mesmo extremamente ferido. "
+                ],
+                proxima: 9,
+            },
+        
+        ]
+    },
+    {
+        enunciado: "Se um Oni estrivesse prestes a morrer o que você faria?",
+        alternativas: [
+            {
+                texto: "Teria piedade",
+                afirmacao: [
+                    "O Tanjiro Kamado. Você é alguém gentil e determinado. Você tem uma personalidade forte com o objetivo de exterminar qualquer ameaça aos humanos como onis, faria tudo para salvar seus amigos"
+                ],
+            },
+            {
+                texto: "O exterminar",
+                afirmacao: [
+                    "O Inosuke Hashibira. Você é uma pessoas bem extravagante, voce é conhecido pelo seu temperamento e que atacaria uma ameaça com tudo de si sem nem pensar, tentaria acabar logo com o inimigo mesmo extremamente ferido."
+                ],
+                proxima: 10,
+            },
+        ]
+    }
+]
